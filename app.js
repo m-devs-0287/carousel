652c39409654c6ca9d5e7c07f9c5fa6984073d80
Manuel D. Nunez <113308860+m-devs-0287@users.noreply.github.com>
2023-01-18 23:32:39 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const imgs = document.getElementById('imgs')
+const leftBtn= document.getElementById('left')
+const rightBtn= document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs .img')
+
+let i = 0
+let interval = setInterval(run, 2000);
+
+function run(){
+  i++
+
+  changeImg()
+}
+
+function changeImg(){
+  if(i > img.length - 1){
+    i = 0
+  }else if(i < 0){
+    i = img.length - 1
+  }
+
+  imgs.style.transform = `translateX(${-i * 500}px)`
+}
+
+rightBtn.addEventListener('click', ()=>{
+  i++
+
+  changeImg()
+  resertInterval()
+})
+
+leftBtn.addEventListener('click', ()=>{
+  i--
+
+  changeImg()
+  resertInterval()
+})
+
+function resertInterval() {
+  clearInterval(interval)
+  interval = setInterval(run, 2000)
+}
+
